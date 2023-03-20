e3d8e79c8f7a88286932dbdda3df0e958f95b4a6
it22283894 <128390727+it22283894@users.noreply.github.com>
2023-03-20 19:37:00 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+   int mark1,mark2;//declaring variables
+   float avg;
+   
+   //get the user inputs
+   printf("Enter the mark1:");
+   scanf("%d",&mark1);
+   
+   printf("Enter the mark2:");
+   scanf("%d",&mark12);
+   
+   avg=mark1+mark2;
+   printf("Aerage is:%.2f\n",avg);
   
   return 0;
 }
